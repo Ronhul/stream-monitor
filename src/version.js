4159befdc805c47d8f43b354ba9b1d000c3971bf
Ronhul <info@ronhul.xyz>
2025-05-27 13:55:43 +0100

    Update version

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // This file contains the version information for the StreamMonitor app
 // The version is automatically updated from package.json
-export const VERSION = '0.3.0';
+export const VERSION = '0.4.0';
 export const BUILD_DATE = '2025-05-27';
